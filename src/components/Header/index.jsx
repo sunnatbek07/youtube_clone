66f9d99d235892417be116b9e908853be62907e3
sunnatbek07 <sunnatbekqadamov9@gmail.com>
2023-12-23 16:23:25 +0500

    Finished header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import YoutubeLogo from "../Icons/YoutubeLogo";
-
-import './style.scss';
 import Search from './../Icons/Search';
+import { Link } from 'react-router-dom';
+import './style.scss';
+import SignIn from './../Icons/SignIn';
 
 const Header = () => {
     return (
         <header>
             <div className='flex items-center gap-6'>
                 <i className='bx bx-menu text-3xl'></i>
-                <YoutubeLogo />
+                <Link to={'/'}>
+                    <YoutubeLogo />
+                </Link>
             </div>
-            <div className='flex items-center'>
+            <div className='flex items-center gap-x-4'>
                 <div className='searchBox'>
                     <div className='searchInp'>
                         <input type="text" placeholder='Search' />
@@ -19,10 +22,17 @@ const Header = () => {
                     <button className='searchBtn'>
                         <Search />
                     </button>
-                    <button className='microphoneBtn'>
-                        <i class='bx bxs-microphone'></i>
-                    </button>
                 </div>
+                <button className='microphoneBtn'>
+                    <i class='bx bxs-microphone'></i>
+                </button>
+            </div>
+            <div className='settings'>
+                <i class='bx bx-dots-vertical-rounded text-2xl cursor-pointer'></i>
+                <button>
+                    <SignIn />
+                    <span>Sign in</span>
+                </button>
             </div>
         </header>
     )
